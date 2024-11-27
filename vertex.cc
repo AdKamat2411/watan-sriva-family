bbf1458bb1470bb7f323a14132bba0ba216ec491
santosh-00 <santoshreddy6850@gmail.com>
2024-11-26 21:04:05 -0500

    done

--- vertex.cc
+++ vertex.cc
@@ -1,24 +1,38 @@
 #include "vertex.h"
 #include <iomanip>
 
-Vertex::Vertex(int Idx, string owner, int houseLevel) : Idx(Idx), 
-    owner(owner), houseLevel(houseLevel) {}
-
-void Vertex::upgradeCriterion() { houseLevel++; }
+Vertex::Vertex(int Idx, string owner, string houseLevel)
+    : Idx(Idx), owner(owner), houseLevel(houseLevel) {}
 
 void Vertex::setOwner(string s) {
     owner = s;
 }
 
-int Vertex::getIdx() const { return Idx; }
+void Vertex::setHouseLevel(const string& level) {
+    houseLevel = level;
+}
 
-string Vertex::getName() const { return owner; }
+string Vertex::getHouseLevel() const {
+    return houseLevel;
+}
 
-ostream& operator<<(ostream& out, const Vertex &v) {
-    if (v.getName() == "") {
+int Vertex::getIdx() const {
+    return Idx;
+}
+
+string Vertex::getName() const {
+    return owner;
+}
+
+bool Vertex::isAvailabale() const {
+    return owner.empty();
+}
+
+ostream& operator<<(ostream& out, const Vertex& v) {
+    if (v.getName().empty()) {
         out << std::setw(2) << std::right << v.getIdx();
     } else {
-        out << v.getName();
+        out << v.getName() << " (" << v.getHouseLevel() << ")";
     }
     return out;
 }
